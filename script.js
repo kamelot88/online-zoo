fc32bb0216a3f849d7412ccb4af465fa8f521450
kamelot88 <VasilchenkoOV1988@gmail.com>
2022-02-15 11:32:41 +0200

    add script burger-menu

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
 //! Бургер меню
-// var burgerLine = document.getElementsByClassName('burger-line');
-// var btnBurger = document.querySelector('.burger');
-// var mainMenu = document.querySelector('.main-menu');
-// var lineBurger1 = document.querySelector('.burger-line:nth-child(1)');
-// var lineBurger2 = document.querySelector('.burger-line:nth-child(2)');
-// var lineBurger3 = document.querySelector('.burger-line:nth-child(3)');
-// var lineBurger4 = document.querySelector('.burger-line:nth-child(4)');
+var btnBurger = document.querySelector('.burger');
+var navMenu = document.querySelector('.nav-menu');
 
-// btnBurger.onclick = function() {
-//     // console.log(mainMenu.style.right);
-//     if(mainMenu.style.right != 0 + "vw") {
-//         mainMenu.style.right = 0 + "vw";
-//         lineBurger2.classList.add('opacity');
-//         lineBurger3.classList.add('opacity');
-//         lineBurger1.classList.add('lineBurger1');
-//         lineBurger4.classList.add('lineBurger4');
-//         // burgerLine.style.boxShadow = "0px 0px 1px 1px white";
-//         for(let elem of burgerLine) {
-//             elem.style.boxShadow = "0px 0px 1px 1px white";
-//         }
-//     } else {
-//         mainMenu.style.right = -60 + "vw";
-//         lineBurger2.classList.remove('opacity');
-//         lineBurger3.classList.remove('opacity');
-//         lineBurger1.classList.remove('lineBurger1');
-//         lineBurger4.classList.remove('lineBurger4');
-//         for(let elem of burgerLine) {
-//             elem.style.boxShadow = 'none';
-//         }
-//     }
-// };
+btnBurger.onclick = function() {
+    if(navMenu.style.display == "block") {
+        navMenu.style.display = "none";
+    } else {
+        navMenu.style.display = "block";
+        }
+    };
